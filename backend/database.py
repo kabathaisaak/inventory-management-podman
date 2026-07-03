e08e69bdf349676bae87ee7729d3f7e674e1be80
kabathaisaak <kabathaisaak@gmail.com>
2026-07-03 12:59:54 +0300

    committed the changes

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,30 @@
+from contextlib import contextmanager
 import psycopg2
+
 from config import Config
 
-def get_connection():
-    return psycopg2.connect(
+
+@contextmanager
+def get_cursor():
+
+    conn = psycopg2.connect(
         host=Config.DB_HOST,
         database=Config.DB_NAME,
         user=Config.DB_USER,
         password=Config.DB_PASSWORD,
         port=Config.DB_PORT
     )
+
+    cur = conn.cursor()
+
+    try:
+        yield cur
+        conn.commit()
+
+    except Exception:
+        conn.rollback()
+        raise
+
+    finally:
+        cur.close()
+        conn.close()
--- backend/validators/product_validator.py
+++ backend/validators/product_validator.py
@@ -1,25 +1,36 @@
+from exceptions.validation import ValidationException
+
+
 def validate_product(data):
+
     if not data:
-        return "Request body is required"
+        raise ValidationException(
+            "Request body is required"
+        )
 
     if "name" not in data:
-        return "Product name is required"
+        raise ValidationException(
+            "Name is required"
+        )
+
+    if not str(data["name"]).strip():
+        raise ValidationException(
+            "Name cannot be empty"
+        )
 
     if "price" not in data:
-        return "Product price is required"
-
-    if not isinstance(data["name"], str):
-        return "Product name must be a string"
-
-    if data["name"].strip() == "":
-        return "Product name cannot be empty"
+        raise ValidationException(
+            "Price is required"
+        )
 
     try:
         price = float(data["price"])
     except (ValueError, TypeError):
-        return "Product price must be a number"
+        raise ValidationException(
+            "Price must be a number"
+        )
 
     if price <= 0:
-        return "Product price must be greater than zero"
-
-    return None
+        raise ValidationException(
+            "Price must be greater than zero"
+        )
